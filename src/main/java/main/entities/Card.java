6c3361f2ca293e8ad669db435736efecd70613f2
Mugenor <lizaklokova@mail.ru>
2017-10-29 01:05:57 +0300

    D1

--- src/main/java/main/entities/Card.java
+++ src/main/java/main/entities/Card.java
@@ -20,4 +20,52 @@ public class Card {
     private Skill skill;
     @Column(name = "cubeNumber")
     private int cubeNumber;
+
+    public void setName (String name) {this.name = name;}
+    public String getName () {return name;}
+    public void setDescription (String description) {this.description = description;}
+    public String getDescription () {return description;}
+    public Creature getCreature() {
+        return creature;
+    }
+    public void setCreature(Creature creature) {
+        this.creature = creature;
+    }
+    public Skill getSkill() {
+        return skill;
+    }
+    public void setSkill(Skill skill) {
+        this.skill = skill;
+    }
+    public int getCubeNumber() {
+        return cubeNumber;
+    }
+    public void setCubeNumber(int cubeNumber) {
+        this.cubeNumber = cubeNumber;
+    }
+    public static void main(String [] args) {
+        Card card = new Card("name", "desc", new Creature(), new Skill(), 5);
+        System.out.println(card.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", creature=" + creature +
+                ", skill=" + skill +
+                ", cubeNumber=" + cubeNumber +
+                '}';
+    }
+
+    public Card() {}
+    public Card (String name, String description, Creature creature, Skill skill, int cubeNumber) {
+        this.name = name;
+        this.description = description;
+        this.creature = creature;
+        this.skill = skill;
+        this.cubeNumber = cubeNumber;
+    }
 }
