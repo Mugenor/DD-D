55eb448bdd738113756df1fb78ad0340fc67976b
Mugenor <lizaklokova@mail.ru>
2017-10-28 21:54:42 +0300

    Norm Lisa

--- src/main/java/main/entities/Top.java
+++ src/main/java/main/entities/Top.java
@@ -1,6 +1,13 @@
 package main.entities;
 
+import javax.persistence.*;
+
+@Entity (name = "top")
 public class Top {
+    @Id
+    @Column
     private Integer position;
+    @OneToOne
+    @JoinColumn (name = "users")
     private User user;
 }
--- src/main/java/main/entities/User.java
+++ src/main/java/main/entities/User.java
@@ -1,11 +1,31 @@
 package main.entities;
 
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity (name = "user")
 public class User {
+    @Id
+    @Column
     private String login;
+    @Column
     private long password;
+    @Column
     private String username;
+    @Column
     private int status;
+    @Column
     private double winrate;
+    @Column
     private long amountGames;
+    @Column
     private int amountWin;
+    @ManyToMany
+    @JoinTable (name = "friends",
+            joinColumns = @JoinColumn (name = "user1",
+                    referencedColumnName = "login"),
+            inverseJoinColumns = @JoinColumn (name = "user2",
+                    referencedColumnName = "login"))
+    private Collection<User> friends;
+
 }
