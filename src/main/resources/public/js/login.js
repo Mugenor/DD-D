6d186d619e8d4fc9786fabdb9e3fe7b1b25b2625
Mugenor <ilya.cherno8@mail.ru>
2018-07-01 20:27:42 +0300

    login and logout buttons

---
+++ src/main/resources/public/js/login.js
@@ -0,0 +1,24 @@
+$(function () {
+    let errorDiv = $('<div/>', {
+        id: 'error',
+        class: 'alert alert-danger',
+    });
+    let errorMessage = $('<p/>').html('Неправильный логин или пароль!');
+    errorDiv.append(errorMessage.html());
+    $("#loginButton").click(function () {
+        let userCredentials = { username: $("#userName").val(), password: $("#password").val()};
+        $.ajax({
+            url: "/auth/login",
+            contentType: "application/json",
+            method: "POST",
+            data: JSON.stringify(userCredentials),
+            success: function(resp){
+                Cookies.set('iplanetDirectoryPro', resp.tokenId);
+                window.location.href = '//localhost:8080/game.html';
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                errorDiv.appendTo('#password-div');
+            }
+        });
+    })
+});
